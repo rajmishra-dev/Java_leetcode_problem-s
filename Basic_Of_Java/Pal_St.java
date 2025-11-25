c08a030746b51a741ee5955550d3d3585e4ee338
Raj Mishra <shradkumar002@gmail.com>
2025-11-25 16:27:02 +0000

    String Palindrome Program added

---
+++ Basic_Of_Java/Pal_St.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Pal_St {
+    public static void main(String[] args)
+    {
+        Scanner sc =new Scanner(System.in);
+        String Input = sc.nextLine();
+        String reversed = new StringBuilder(Input).reverse().toString();
+        System.out.println("IS Palindrom :" + Input.equals(reversed));
+        
+    }
+}
